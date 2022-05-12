b082e93fc8a7f91a563e0a0f935ff6527c676e8b
Alexandre Calaça <alexcalaca@gmail.com>
2022-05-12 14:47:08 -0300

    Insert retracting cards feature

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const panels = document.querySelectorAll('.panel')
 console.log(panels)
 
 panels.forEach((panel) => {
-	panel.addEventListener('click', () => {		
+	panel.addEventListener('click', () => {
+		removeActiveClasses()
 		panel.classList.add('active')
 	})
 })
+
+function removeActiveClasses(){
+	panels.forEach(panel => {
+		panel.classList.remove('active')
+	})
+}
